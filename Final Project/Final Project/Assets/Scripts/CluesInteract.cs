fa6e6a155c82d90a69c9ae7203fb10c9f0083f9a
Richard Adams <radams75@gatech.edu>
2021-04-29 22:55:39 -0400

    test commit 2

--- Final Project/Final Project/Assets/Scripts/CluesInteract.cs
+++ Final Project/Final Project/Assets/Scripts/CluesInteract.cs
@@ -27,36 +27,42 @@ public class CluesInteract : MonoBehaviour
     }
     void Update() {
         if (Input.GetKeyDown(KeyCode.E) && onTrigger) {
+            if (this.gameObject.tag == "Table") {
+                    textBox.GetComponent<Text>().text = "";
+                    captionBox.GetComponent<Text>().text = " 'Nothing too relevant here. I should keep looking.' ";
+                }
+
             if (!isReading) {
                 customImage.enabled = true;
                 textBox.GetComponent<Text>().text = "";
                 isReading = true;
                 if (this.gameObject.tag == "ActivityLog") {
-                    captionBox.GetComponent<Text>().text = "They have been tracking my activity...everyday...";
+                    captionBox.GetComponent<Text>().text = " 'They have been tracking my activity...everyday...' ";
                     clueCount(clue1);
                     clue1 = true;
                 } else if (this.gameObject.tag == "Sitting") {
-                    captionBox.GetComponent<Text>().text = "This is...me? Ok this is getting weird and slightly terrifying";
+                    captionBox.GetComponent<Text>().text = " 'This is...me? Ok this is getting weird and slightly terrifying' ";
                     clueCount(clue2);
                     clue2 = true;
                 } else if (this.gameObject.tag == "Running") {
-                    captionBox.GetComponent<Text>().text = "Me again. What the hell is going on here.";
+                    captionBox.GetComponent<Text>().text = " 'Me again. What the hell is going on here.' ";
                     clueCount(clue3);
                     clue3 = true;
                 } else if (this.gameObject.tag == "Blueprint") {
-                    captionBox.GetComponent<Text>().text = "It seems they were going over the blueprints to my house";
+                    captionBox.GetComponent<Text>().text = " 'It seems they were going over the blueprints to my house' ";
                     clueCount(clue4);
                     clue4 = true;
                 } else if (this.gameObject.tag == "Bench") {
-                    captionBox.GetComponent<Text>().text = "There must be a reason these guys were following my life so closely. I have to get to the bottom of this.";
+                    captionBox.GetComponent<Text>().text = " 'There must be a reason these guys were following my life so closely. I have to get to the bottom of this.' ";
                     clueCount(clue5);
                     clue5 = true;
-                }
+                } 
             } else {
                 customImage.enabled = false;
                 isReading = false;
                 captionBox.GetComponent<Text>().text = "";
             }
+            
         }
 
         //StartCoroutine(queElevator());
@@ -69,6 +75,7 @@ public class CluesInteract : MonoBehaviour
 
     public void OnTriggerExit(Collider other) {
         textBox.GetComponent<Text>().text = "";
+        captionBox.GetComponent<Text>().text = "";
         customImage.enabled = false;
         onTrigger = false;
         StartCoroutine(queElevator());
